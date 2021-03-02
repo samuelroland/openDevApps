2b885a9e7c301971a81fa3f55701d9993f6b51d1
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-03-02 23:13:16 +0100

    redesign entirely the Popup.vue

--- extension/popup/src/components/Popup.vue
+++ extension/popup/src/components/Popup.vue
@@ -1,27 +1,46 @@
 <template>
-  <div>
-    <div class="flex">
-      <div class="flex">
+  <div class="bg-blue-300 w-40 p-1">
+    <div class="flex w-full">
+      <div class="flex flex-1">
         <h3 class="flex flex-row items-center m-0">
-          open<img src="icons/icon-48.png" alt="" />evD
+          open <img src="icons/icon-48.png" class="w-4 " alt="" />evD
         </h3>
-        <span>v0.1</span>
+        <span class="text-xs flex items-end ml-1">v0.1</span>
       </div>
-      <span
-        ><img src="icons/code.svg" alt=""/> <img src="icons/help.svg" alt=""
+      <span class="flex"
+        ><img
+          src="icons/code.svg"
+          class="w-5 hover:bg-blue-500 rounded hover:text-white px-1"
+          alt=""/>
+        <img
+          src="icons/help.svg"
+          alt=""
+          class="w-5 hover:bg-blue-500 rounded hover:text-white px-1"
       /></span>
     </div>
-    <ul class="list-none pl-5">
-      <li v-for="item in items" :key="item.id">
-        <a :href="item.link">{{ item.name }}</a>
-      </li>
-      <li>
-        DevDashboard
-        <span
-          ><img src="icons/home.svg" alt=""/><img
-            src="icons/arrow-circle-up.svg"
-            alt=""
-        /></span>
+    <ul class="list-none">
+      <li
+        v-for="item in items"
+        :key="item.id"
+        class="text-xs hover:bg-blue-400 rounded-sm border my-1 border-solid border-blue-600"
+      >
+        <a
+          :href="'http://' + item.link"
+          class="hover:text-white w-full block px-1"
+          >{{ item.name }}
+          <span>
+            <img
+              src="icons/home.svg"
+              class="w-2 inline"
+              alt=""
+              v-if="item.local"
+            />
+            <img
+              src="icons/arrow-circle-up.svg"
+              class="w-2 inline"
+              alt=""
+            /> </span
+        ></a>
       </li>
     </ul>
   </div>
