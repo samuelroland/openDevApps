fe381de5cd58dd7d01622f6279b7ee4be61d0310
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-03-02 22:34:14 +0100

    created structure and imported popup.html in Popup.vue

---
+++ extension/popup/src/components/Popup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="flex">
+      <div style=" display: flex;">
+        <h3
+          style="display: flex;flex-direction: row;align-items: center; margin:0;"
+        >
+          open<img src="icons/icon-48.png" alt="" />evD
+        </h3>
+        <span>v0.1</span>
+      </div>
+      <span
+        ><img src="icons/code.svg" alt=""/> <img src="icons/help.svg" alt=""
+      /></span>
+    </div>
+    <ul>
+      <li>
+        DevDashboard
+        <span
+          ><img src="icons/home.svg" alt=""/><img
+            src="icons/arrow-circle-up.svg"
+            alt=""
+        /></span>
+      </li>
+      <li>E-commerce in dev</li>
+      <li>localhost:8085</li>
+      <li>GitHub Profile</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Popup",
+  props: {
+    msg: String,
+  },
+};
+</script>
--- extension/popup/src/main.js
+++ extension/popup/src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+import "../public/style.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
