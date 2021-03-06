e3dca91873a87ba356a2c80284b813eaae5a6ad9
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-03-06 18:49:03 +0100

    import files after vue CLI browser-extension run
    removed src/App.vue and src/main.js because they exists in src/popup too.

---
+++ extension/src/popup/App.vue
@@ -0,0 +1,20 @@
+<template>
+  Hey great reload !
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld }
+}
+</script>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
