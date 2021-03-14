84aa4addc341f252eb9b19ee5a61bd397e1f5266
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-03-14 17:54:16 +0100

    configure vue.config.js to keep the same content in the generated manifest.json than in src/manifest.json

--- extension/vue.config.js
+++ extension/vue.config.js
@@ -1,14 +1,22 @@
+//Docs here: https://www.npmjs.com/package/vue-cli-plugin-browser-extension
+
 module.exports = {
   pages: {
     popup: {
-      template: 'public/browser-extension.html',
-      entry: './src/popup/main.js',
-      title: 'Popup'
-    }
+      template: "public/browser-extension.html",
+      entry: "./src/popup/main.js",
+      title: "Popup",
+    },
   },
   pluginOptions: {
     browserExtension: {
-      componentOptions: {}
-    }
-  }
-}
+      //Configurations to keep the same content in the generated manifest.json than in src/manifest.json
+      manifestSync: [], //remove version sync (between package.json and generated manifest.json) (default value is ['version'])
+      manifestTransformer: (manifest) => {
+        delete manifest.content_security_policy; //remove the key in generated manifest.json to leave the default content security policy
+        return manifest;
+      },
+      componentOptions: {},
+    },
+  },
+};
